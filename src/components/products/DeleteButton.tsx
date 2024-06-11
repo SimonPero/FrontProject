68e77c0b943dc112156ecb08ab36aea66d6951a4
Simon Pero <simonraulpero@gmail.com>
2024-06-10 23:46:13 -0300

    created a button call the delete http petition by id of the backend

---
+++ src/components/products/DeleteButton.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { Button } from '@/components/ui/button';
+import ProductApi from '@/api/productApi';
+const productApi = new ProductApi();
+interface DeleteButtonProps {
+    productId: string;
+}
+
+const DeleteButton: React.FC<DeleteButtonProps> = ({ productId }) => {
+    const handleClick = async () => {
+        try {
+            await productApi.deleteProd(productId);
+        } catch (error) {
+            console.error('Error deleting product:', error);
+        }
+    };
+
+    return (
+        <Button onClick={handleClick}>
+            Eliminar
+        </Button>
+    );
+}
+
+export default DeleteButton;
