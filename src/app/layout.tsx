b012ec610a059cd766c641d0d616ba09bae5ff70
Simon Pero <simonraulpero@gmail.com>
2024-06-10 23:52:19 -0300

    here i use previousProvider to maintain previousContext actualized

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import PreviousPathnameProvider from "@/components/contexts/goBackContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="bg-blue-500">
+          <h1>popopopo</h1>
+          <div>talalalala</div>
+        </header>
+        <PreviousPathnameProvider>
+          {children}
+        </PreviousPathnameProvider>
+        <footer className="bg-red-500">
+          <h1>popopopo</h1>
+          <div>talalalala</div>
+        </footer>
+      </body>
     </html>
   );
 }
