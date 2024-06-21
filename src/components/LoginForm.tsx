f435570f2e620c6a819a3d32b4cfdba289c362f4
Simon Pero <simonraulpero@gmail.com>
2024-06-20 22:16:04 -0300

    added shadcn form, input and modified a bit of the code to create forms for registering a logging in

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,16 +1,83 @@
+"use client"
 import { Button } from "./ui/button"
 import { doSocialLogin } from "@/app/actions"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import Link from "next/link";
 
-const LoginForm = () => {
+const formSchema = z.object({
+    email: z.string()
+        .min(1, "Email is required")
+        .email("Invalid email"),
+    password: z.string()
+        .min(1, "Password is required")
+        .min(8, "Invalid password"),
+})
+
+
+export function LoginForm() {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        //tomorrow wil do the manual 
+        console.log(values)
+    }
     return (
-        <form action={doSocialLogin}>
-            <Button type="submit" name="action" value="google">
-                Sign In With Google
-            </Button>
-            <Button type="submit" name="action" value="github">
-                Sign In With GitHub
-            </Button>
-        </form>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input placeholder="example@gmail.com" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input placeholder="password" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+            <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
+                Or
+            </div>
+
+            <form action={doSocialLogin}>
+                <Button type="submit" name="action" value="google" className="mx-auto my-4 flex w-full items-center justify-evenly">
+                    Sign In With Google
+                </Button>
+            </form>
+
+            <p className="text-center text-sm text-gray-600 mt-2">If you don&apos;t have an account, please&nbsp;
+                <Link className="text-blue-500 hove:underline" href={"/signUp"}>Sing Up</Link>
+            </p>
+
+        </Form>
     )
 }
-export default LoginForm
---
+++ src/components/RegisterForm.tsx
@@ -0,0 +1,148 @@
+"use client"
+import { Button } from "./ui/button"
+import { doSocialLogin, doSocialRegister } from "@/app/actions"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import Link from "next/link";
+
+const formSchema = z.object({
+    email: z.string()
+        .min(1, "Email is required")
+        .email("Invalid email"),
+    surname: z.string()
+        .min(1, "Surname is required"),
+    password: z.string()
+        .min(1, "Password is required"),
+    phone: z.string()
+        .min(1, "Phone is required"),
+    age: z.number()
+        .min(1, "Age is required")
+        .gte(13, "You must be older than 13 years old")
+        .lte(90, "You must be younger than 90 years old"),
+    location: z.string()
+        .min(1, "Location is required"),
+
+})
+
+export function RegisterForm() {
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            surname: "",
+            password: "",
+            phone: "",
+            age: 0,
+            location: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        //tomorrow wil do the manual 
+        console.log(values)
+    }
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Example@gmail.com" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="surname"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Surname</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Enter your surname" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Enter your password" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="phone"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Phone</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Enter your phone" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="age"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Age</FormLabel>
+                            <FormControl>
+                                <Input type="number" placeholder="Enter your age" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="location"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Location</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Enter your location" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+            <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
+                Or
+            </div>
+
+            <form action={doSocialRegister}>
+                <Button type="submit" name="action" value="google" className="mx-auto my-4 flex w-full items-center justify-evenly">
+                    Sign Up With Google
+                </Button>
+            </form>
+
+            <p className="text-center text-sm text-gray-600 mt-2">Do you already have an account? please&nbsp;
+                <Link className="text-blue-500 hove:underline" href={"/signUp"}>Sing In</Link>
+            </p>
+
+        </Form>
+    )
+}
