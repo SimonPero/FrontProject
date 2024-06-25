a59eb8b6c0666fe11c56f2d80faf48fd374914b7
Simon Pero <simonraulpero@gmail.com>
2024-06-24 22:08:21 -0300

    i will have to standarize the use of apiClient, axios interceptor, and review its quality

--- src/api/productApi.ts
+++ src/api/productApi.ts
@@ -1,11 +1,13 @@
+import apiClient from "@/apiClient";
+
 export default class ProductApi {
     async getData() {
-        const res = await fetch('http://localhost:8080/api/products', { cache: 'no-store' });
-        if (!res.ok) {
+        const res = await apiClient.get('/api/products');
+        if (res.status !== 200) {
             // This will activate the closest `error.js` Error Boundary
             throw new Error('Failed to fetch data');
         }
-        return res.json();
+        return res.data;
     }
     async getDataById(id: string) {
         const res = await fetch(`http://localhost:8080/api/products/${id}`, { cache: 'no-store' });
@@ -36,18 +38,28 @@ export default class ProductApi {
 
     async deleteProd(id: string) {
         try {
-            const res = await fetch(`http://localhost:8080/api/products/${id}`, {
-                method: 'DELETE',
-                cache: 'no-store'
-              });
-            if (!res.ok) {
+            const res = await apiClient.delete(`/api/products/${id}`)
+            if (res.status !== 200) {
                 throw new Error('Failed to delete prod');
             }
-            return res.url;
+            return res.config.url;
         } catch (error) {
             // Maneja cualquier error aquí
             console.error('Error deleting prod:', error);
             throw error;
+        }
+    }
+
+    async updateProd(id: string) {
+        try {
+            const response = await apiClient.put(`/api/products/${id}`);
+            if (response.status !== 200) {
+                throw new Error('Failed to submit the form');
+            }
+            return response.data; // Retorna los datos de respuesta si es necesario
+        } catch (error) {
+            console.error('Error submitting form:', error);
+            throw error; // Lanza el error para que sea manejado por el llamador
         }
     }
 }
