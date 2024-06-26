367aa81e5280d841f82130d3af803264be2f70b4
Simon Pero <simonraulpero@gmail.com>
2024-06-26 20:08:49 -0300

    added doCrendetialLogin actions so crendential provider works

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -4,11 +4,27 @@ import { signIn } from "@/auth"
 
 export async function doSocialLogin(formData: any) {
     const action = formData.get("action")
-    await signIn(action, {redirectTo: "/"})
+    await signIn(action, { redirectTo: "/" })
 }
 
 //elimie doSocialRegister
 
 export async function doLogout() {
-    
+
+}
+
+export async function doCredentialLogin(formData: any) {
+    try {
+        const res = await signIn("credentials", {
+            email: formData.email,
+            password: formData.password,
+            redirect: false
+        });
+        if (res.error) {
+            throw new Error(res.error);
+        }
+        return res;
+    } catch (error: any) {
+        throw new Error(error.message);
+    }
 }
