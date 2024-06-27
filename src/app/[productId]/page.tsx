bd47683dc6fc250e8fa676e837ad8c9362c4f114
Simon Pero <simonraulpero@gmail.com>
2024-06-27 02:33:33 -0300

    added a way to only see the product detail you choose

---
+++ src/app/[productId]/page.tsx
@@ -0,0 +1,18 @@
+import Product from "@/components/products/Product";
+import ProductApi from "@/api/productApi";
+import { BackLink } from "@/components/contexts/goBackContext";
+const productApi = new ProductApi();
+
+export default async function ProductModify({
+    params,
+}: {
+    params: { productId: string }
+}) {
+    const data = await productApi.getDataById(params.productId)
+    return (
+        <section className="space-y-4 m-5">
+            <BackLink />
+            <Product {...data} />
+        </section>
+    );
+}
