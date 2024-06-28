04275a53f8acac4d93dd5d8597ef65da6e195a75
Simon Pero <simonraulpero@gmail.com>
2024-06-28 15:36:10 -0300

    replaced the previuosPathName context with the native tool of nextjs

---
+++ src/components/useful/goBackContext.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { Button } from "../ui/button";
+
+export function BackLink() {
+    const router = useRouter();
+    const handleBack = () => {
+        router.back()
+    }
+    return (
+        <Button onClick={handleBack}>
+            pololo
+        </Button>
+    );
+}
