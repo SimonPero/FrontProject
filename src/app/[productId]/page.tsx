a15053b452b4c1a638e870cf556a8e0713e63dda
Simon Pero <simonraulpero@gmail.com>
2024-07-02 23:22:39 -0300

    minor changes

--- src/app/[productId]/page.tsx
+++ src/app/[productId]/page.tsx
@@ -2,6 +2,8 @@ import Product from "@/components/products/Product";
 import ProductApi from "@/api/productApi";
 import { BackLink } from "@/components/useful/goBackContext";
 import { auth } from "@/auth";
+import AddToCartButton from "@/components/cart/addToCartButton";
+import { SessionProvider } from "next-auth/react";
 const productApi = new ProductApi();
 
 export default async function ProductModify({
@@ -11,10 +13,14 @@ export default async function ProductModify({
 }) {
     const session = await auth();
     const data = await productApi.getDataById(params.productId, session)
+
     return (
-        <section className="space-y-4 m-5">
-            <BackLink />
-            <Product {...data} />
-        </section>
+        <SessionProvider>
+            <section className="space-y-4 m-5">
+                <BackLink />
+                <Product {...data} />
+                <AddToCartButton productID={params.productId} email={session?.user?.email} quantity={"1"} />
+            </section>
+        </SessionProvider>
     );
 }
