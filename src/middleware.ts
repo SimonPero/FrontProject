f5562a428341bd2e70bfab9c132a66b34c2e4031
Simon Pero <simonraulpero@gmail.com>
2024-08-18 23:38:48 -0300

    changed to work as intended in the documentation

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,20 @@
-import { NextResponse } from 'next/server';
-import { NextRequest } from 'next/server';
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { auth } from "./auth";
-
-
-const protectedRoutes = {
-  '/productManagement': true,
-  '/cart': true,
-  '/profile': true,
-  '/orders': true,
-};
 
 export default async function middlewareAuth(req: NextRequest) {
   const session = await auth();
   const { pathname } = req.nextUrl;
 
-  if (pathname === '/signIn') return NextResponse.next();
+  if (pathname === "/signIn") return NextResponse.next();
 
-  for (const [route, isProtected] of Object.entries(protectedRoutes)) {
-    if (pathname.startsWith(route) && isProtected) {
-      if (!session?.jwt) {
-        return NextResponse.redirect(new URL('/signIn', req.url));
-      }
-      break;
-    }
+  if (!session?.jwt) {
+    return NextResponse.redirect(new URL("/signIn", req.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ["/productManagement", "/cart", "/products/:productId*"],
 };
